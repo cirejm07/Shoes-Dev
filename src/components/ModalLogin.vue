d7693c82388de679c7680245e205e789f0d97347
windev22 <irwingatbonton@gmail.com>
2022-08-06 14:24:34 +0800

    button in nav

--- src/components/ModalLogin.vue
+++ src/components/ModalLogin.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="">
-  <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+  <button type="button" class="border-none no-underline hover:underline text-base inline-block px-6 py-2.5 font-semibold text-xs leading-tight uppercase rounded focus:outline-none focus:ring-0 text-blue-600 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
     Signup
   </button>
   
-  <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalCenteredScrollable">
+  <button type="button" class="border-none no-underline mx-2 hover:underline text-base inline-block px-6 py-2.5 font-semibold text-xs leading-tight uppercase rounded focus:outline-none focus:ring-0 text-blue-600 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModalCenteredScrollable">
     Login
   </button>
 </div>
